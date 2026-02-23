2e805a41d9929d1be42032498faf5460b2c210af
igor <ivanovil.121.54@gmail.com>
2026-02-23 14:56:01 +0300

    Доработка

--- ident.php
+++ ident.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Идентификация пользователя</title>
-    <link rel="stylesheet" href="/css/style1.css">
+    <link rel="stylesheet" href="/css/style_main.css">
 </head>
 
 <body>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style_main.css">
+    <title>Масленица в Березниках</title>
+</head>
+
+<body>
+
+<div class="header">
+    <div class="custom-button" role="button" tabindex="0" onclick="document.location.href = 'ident.php'">
+        Войти
+    </div>
+</div>
+<div class="main">
+</div>
+<div class="footer"></div>
+</body>    
+</html>
