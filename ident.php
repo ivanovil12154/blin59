c6b214fe1ccc15d7e6c87ea30d7b9d50aff5224a
igor <ivanovil.121.54@gmail.com>
2026-02-23 11:43:39 +0300

    begin

---
+++ ident.php
@@ -0,0 +1,21 @@
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Идентификация пользователя</title>
+    <link rel="stylesheet" href="/css/style1.css">
+</head>
+
+<body>
+<div class="container_av">
+    <h3 class="heading_av">Авторизуйтесь, пожалуйста!</h3> 
+  <form method="post" name="iden">
+    <input type="text" name="login"  class="form-control" placeholder="Ваш логин"/><br />
+    <input type="password" name="pass"  class="form-control"  id="pass" placeholder="Ваш пароль"><br>	
+    <input type="submit" class="btn-av" value="Продолжить" onclick="WM_IdentonKeySend()" />	
+  </form>
+</div>
+<script src="js/wm_ident.js"></script>
+</body>
+</html>
